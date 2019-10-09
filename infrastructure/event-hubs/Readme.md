64f700530ca11f1253b6b86fe7b0fc18ce0acc5d
Gramoz Krasniqi <Gramoz.Krasniqi@conplement.de>
2019-10-09 15:58:41 +0200

    Readme file for infrastructure

--- infrastructure/event-hubs/Readme.md
+++ infrastructure/event-hubs/Readme.md
@@ -1,9 +1,9 @@
-docker build -t eventhubsinit:latest
-docker run -it --rm eventshubsinit:latest
+# Creating event hubs instance
 
-az login
-set right subscription
-
-run ./eventhubs-init.sh
+* Copy `.env.template` to `.env` and change the values as you see fit 
+* Run `docker build -t eventhubsinit:latest .`
+* Run `docker run -it --rm --env-file .env eventhubsinit:latest`
+* After landing in the container shell run `az login` and login to azure
+* Run `./eventhubs-init.sh`. At the end it will pring the credentials that you will need to put on the `.env` file on the root project
 
 Event Hubs namespace needs to be Standard, Basic dosent support Kafka
--- infrastructure/event-hubs/eventhubs-init.sh
+++ infrastructure/event-hubs/eventhubs-init.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-location=westeurope
-rgName=eventhub-we-rg
-eventHubName=testEventHub
-eventHubNamespace=testEventHubNamespaceGzKras
-
 #Create RG
-az group create -l $location -n $rgName
+az group create -l $LOCATION -n $RESOURCE_GROUP_NAME
 
 #Create Event Hubs namespace
-az eventhubs namespace create -l $location -g $rgName -n $eventHubNamespace --enable-kafka true --sku Standard
+az eventhubs namespace create -l $LOCATION -g $RESOURCE_GROUP_NAME -n $EVENT_HUB_NAMESPACE --enable-kafka true --sku Standard
 
 #Create EventHub
-az eventhubs eventhub create -g $rgName -n $eventHubName --namespace-name $eventHubNamespace
+az eventhubs eventhub create -g $RESOURCE_GROUP_NAME -n $EVENT_HUB_NAME --namespace-name $EVENT_HUB_NAMESPACE
 
-#Create Authorization rule to publish and subscribe
+#Output keys for connection to event hubs instance
+printf "\n Printing credentials \n"
+az eventhubs namespace authorization-rule keys list --namespace-name $EVENT_HUB_NAMESPACE -n RootManageSharedAccessKey -g $RESOURCE_GROUP_NAME
